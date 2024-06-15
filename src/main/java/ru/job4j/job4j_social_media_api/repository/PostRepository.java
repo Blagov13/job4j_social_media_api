6e5eda905a1108aa5497d1c6bbcd78458161aac8
aleksandrblagov <blagov13rus@gmail.com>
2024-06-15 16:33:50 +0300

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/job4j_social_media_api/repository/PostRepository.java
+++ src/main/java/ru/job4j/job4j_social_media_api/repository/PostRepository.java
@@ -1,8 +1,18 @@
 package ru.job4j.job4j_social_media_api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.job4j_social_media_api.model.Post;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findByUserId(Long userId);
+
+    List<Post> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
 
