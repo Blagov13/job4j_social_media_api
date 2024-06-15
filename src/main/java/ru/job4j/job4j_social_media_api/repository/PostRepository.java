b1440b9c1316f5b53e8ccd7f700f6ede3a422fa5
aleksandrblagov <blagov13rus@gmail.com>
2024-06-15 16:58:59 +0300

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/job4j_social_media_api/repository/PostRepository.java
+++ src/main/java/ru/job4j/job4j_social_media_api/repository/PostRepository.java
@@ -2,17 +2,39 @@ package ru.job4j.job4j_social_media_api.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.job4j_social_media_api.model.Post;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByUserId(Long userId);
 
     List<Post> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
 
     Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            update Post post set post.title = :title and post.text = :text
+            where post.id = :id
+            """)
+    int updateTitleAndText(@Param("title") String title, @Param("text") String text, @Param("id") Long id);
+
+    @Modifying
+    @Query("""
+            UPDATE Post p SET p.imageURL = null WHERE p.id = :postId
+            """)
+    void deleteImageByPostId(@Param("postId") Long postId);
+
+    @Modifying
+    @Query("""
+            DELETE FROM Post p WHERE p.id = :postId
+            """)
+    void deletePostById(@Param("postId") Long postId);
 }
 
--- src/main/java/ru/job4j/job4j_social_media_api/repository/UserRepository.java
+++ src/main/java/ru/job4j/job4j_social_media_api/repository/UserRepository.java
@@ -1,8 +1,37 @@
 package ru.job4j.job4j_social_media_api.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import ru.job4j.job4j_social_media_api.model.Post;
 import ru.job4j.job4j_social_media_api.model.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("""
+            select user from User as user
+            where user.username = ?1 and user.passwordHash = ?2
+            """)
+    Optional<User> findByLevelInRange(String username, String passwordHash);
+
+    @Query("""
+            SELECT u.followers FROM User u WHERE u.id = :userId
+            """)
+    List<User> findAllFollowersByUserId(@Param("userId") Long userId);
+
+    @Query("""
+            SELECT u.friends FROM User u WHERE u.id = :userId
+            """)
+    List<User> findAllFriendsByUserId(@Param("userId") Long userId);
+
+    @Query("""
+            SELECT p FROM Post p WHERE p.author IN (SELECT u.followers FROM User u WHERE u.id = :userId) ORDER BY p.createdAt DESC
+            """)
+    Page<Post> findAllPostsFromFollowersByUserIdOrderByCreatedAtDesc(@Param("userId") Long userId, Pageable pageable);
 }
 
