b0d475746e21eac5d578c05463a82384f41c98c1
aleksandrblagov <blagov13rus@gmail.com>
2024-06-19 14:44:05 +0300

    4. Добавление SpringSecurity в проект Social_media [#505121]

--- src/main/java/ru/job4j/job4j_social_media_api/security/controller/AuthController.java
+++ src/main/java/ru/job4j/job4j_social_media_api/security/controller/AuthController.java
@@ -41,7 +41,6 @@ public class AuthController {
                 .body(new MessageResponseDTO(registerDTO.getMessage()));
     }
 
-
     @PostMapping("/signin")
     public ResponseEntity<JwtResponseDTO> authenticateUser(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
         Authentication authentication = authenticationManager
