49b05ff88148a3c803dda114e9d4641a43f3d6a8
aleksandrblagov <blagov13rus@gmail.com>
2024-06-19 12:55:21 +0300

    6. ResponseEntity. Статусы ответов. [#505111 #501172]

--- src/main/java/ru/job4j/job4j_social_media_api/controller/PostController.java
+++ src/main/java/ru/job4j/job4j_social_media_api/controller/PostController.java
@@ -1,21 +1,41 @@
 package ru.job4j.job4j_social_media_api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.job4j.job4j_social_media_api.dto.UserPostsDTO;
 import ru.job4j.job4j_social_media_api.model.Post;
 import ru.job4j.job4j_social_media_api.service.PostService;
 
+import java.util.List;
+
+@Tag(name = "PostController", description = "PostController management APIs")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/post")
 public class PostController {
     private final PostService postService;
 
+    @Operation(
+            summary = "Создать новый пост",
+            description = "Создает новый пост на основе переданных данных.",
+            tags = {"Post", "CreatePost"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Пост успешно создан",
+                    content = {@Content(schema = @Schema(implementation = Post.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос",
+                    content = {@Content(schema = @Schema())})})
     @PostMapping
-    public ResponseEntity<Post> save(@RequestBody Post post) {
+    public ResponseEntity<Post> save(@Valid @RequestBody Post post) {
         postService.create(post);
         var uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -27,26 +47,65 @@ public class PostController {
                 .body(post);
     }
 
+    @Operation(
+            summary = "Получить пост по postId",
+            description = "Получите объект Post, указав его postId. Ответом будет объект Post с postId.",
+            tags = {"Post", "get"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = Post.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema())})})
     @GetMapping("/{postId}")
-    public ResponseEntity<Post> get(@PathVariable long id) {
-        return postService.findById(id)
+    public ResponseEntity<Post> get(@PathVariable Long postId) {
+        return postService.findById(postId)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PatchMapping
-    public ResponseEntity<Void> update(@PathVariable String tittle, String text, Long id) {
-        if (postService.update(tittle, text, id)) {
+    @Operation(
+            summary = "Изменить название и текст поста по postId",
+            description = "Измените объект Post, указав его postId, tittle, text.",
+            tags = {"Post", "update"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = Post.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema())})})
+    @PatchMapping("/{postId}")
+    public ResponseEntity<Void> update(@PathVariable Long postId, @RequestParam String tittle, @RequestParam String text) {
+        if (postService.update(tittle, text, postId)) {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(
+            summary = "Удалить пост по postId",
+            description = "Удалите объект Post, указав его postId.",
+            tags = {"Post", "removeById"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = Post.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema())})})
     @DeleteMapping("/{postId}")
-    public ResponseEntity<Void> removeById(@PathVariable long id) {
-        if (postService.deletePostById(id)) {
+    public ResponseEntity<Void> removeById(@PathVariable Long postId) {
+        if (postService.deletePostById(postId)) {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @Operation(
+            summary = "Получить все посты пользователей по списку userId",
+            description = "Получите список постов пользователей, указав их userId.",
+            tags = {"List", "getUserPosts"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Список постов пользователей",
+                    content = {@Content(schema = @Schema(implementation = UserPostsDTO.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос",
+                    content = {@Content(schema = @Schema())})})
+    @GetMapping("/users/posts")
+    public ResponseEntity<List<UserPostsDTO>> getUserPosts(@RequestParam List<Long> userIds) {
+        List<UserPostsDTO> userPosts = postService.getUserPosts(userIds);
+        if (userPosts.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(userPosts);
     }
 }
--- src/main/java/ru/job4j/job4j_social_media_api/controller/UserController.java
+++ src/main/java/ru/job4j/job4j_social_media_api/controller/UserController.java
@@ -1,5 +1,12 @@
 package ru.job4j.job4j_social_media_api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,14 +15,24 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import ru.job4j.job4j_social_media_api.model.User;
 import ru.job4j.job4j_social_media_api.service.UserService;
 
+@Tag(name = "UserController", description = "UserController management APIs")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
 
+    @Operation(
+            summary = "Создать нового пользователя",
+            description = "Создает нового пользователя на основе переданных данных.",
+            tags = {"User", "CreateUser"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Пользователь успешно создан",
+                    content = {@Content(schema = @Schema(implementation = User.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", description = "Неверный запрос",
+                    content = {@Content(schema = @Schema())})})
     @PostMapping
-    public ResponseEntity<User> save(@RequestBody User user) {
+    public ResponseEntity<User> save(@Valid @RequestBody User user) {
         userService.save(user);
         var uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -27,6 +44,13 @@ public class UserController {
                 .body(user);
     }
 
+    @Operation(
+            summary = "Получить пользователя по userId",
+            description = "Получите объект User, указав его userId. Ответом будет объект User с userId, именем пользователя и датой создания.",
+            tags = {"User", "get"})
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = User.class), mediaType = "application/json")}),
+            @ApiResponse(responseCode = "400", content = {@Content(schema = @Schema())})})
     @GetMapping("/{userId}")
     public ResponseEntity<User> get(@PathVariable int userId) {
         return userService.findById(userId)
@@ -34,6 +58,10 @@ public class UserController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(
+            summary = "Обновить информацию о пользователе",
+            description = "Обновляет информацию пользователя по указанному userId.",
+            tags = {"User", "UpdateUser"})
     @PatchMapping
     public ResponseEntity<Void> update(@PathVariable String username, String email, Long id) {
         if (userService.update(username, email, id)) {
@@ -42,6 +70,10 @@ public class UserController {
         return ResponseEntity.notFound().build();
     }
 
+    @Operation(
+            summary = "Удалить пользователя по userId",
+            description = "Удаляет пользователя по указанному userId.",
+            tags = {"User", "DeleteUser"})
     @DeleteMapping("/{userId}")
     public ResponseEntity<Void> removeById(@PathVariable int userId) {
         if (userService.deleteById(userId)) {
